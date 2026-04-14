4e72510880dc08cc63b9f65bdb2208908ee75dba
Adrián Faz <51978727+fazfaz7@users.noreply.github.com>
2026-04-14 10:14:47 +0200

    Initial Commit

---
+++ Spatial Violin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Spatial Violin
+//
+//  Created by Adrian Emmanuel Faz Mercado on 14/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Spatial Violin/Spatial_ViolinApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spatial_ViolinApp.swift
+//  Spatial Violin
+//
+//  Created by Adrian Emmanuel Faz Mercado on 14/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Spatial_ViolinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
